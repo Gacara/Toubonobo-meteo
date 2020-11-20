949dd892026c61a1bc4162eef4f1d003afb62d22
Wilfred <41619827+MrEquarell@users.noreply.github.com>
2020-11-20 17:40:31 +0100

    add umbrella

--- src/views/model.tsx
+++ src/views/model.tsx
@@ -13,6 +13,7 @@ import Clouds from '../component/clouds';
 import Hat from '../component/clothes/hats/Hat';
 import Mask from '../component/clothes/masks/Mask';
 import Sunglasses from '../component/clothes/sunglasses/Sunglasses';
+import Umbrella from '../component/clothes/umbrella/Umbrella';
 
 interface cameraInterface {
   far: number;
@@ -24,6 +25,7 @@ function ModelViewer(): React.ReactElement{
   const [wearMask, setWearMask] = useState<boolean>(false);
   const [wearHat, setWearHat] = useState<boolean>(false);
   const [wearSunglasses, setWearSunglasses] = useState<boolean>(false);
+  const [wearUmbrella, setWearUmbrella] = useState<boolean>(false);
   const [camera, setCamera] = useState<Partial<ReactThreeFiber.Object3DNode<THREE.Camera, typeof THREE.Camera> & ReactThreeFiber.Object3DNode<THREE.PerspectiveCamera, typeof THREE.PerspectiveCamera> & ReactThreeFiber.Object3DNode<THREE.OrthographicCamera, typeof THREE.OrthographicCamera>>>({ far: 2000, position: [5, 1.2, -18] });
  
   function handleCLick() {
@@ -67,6 +69,7 @@ function ModelViewer(): React.ReactElement{
           <Hat visible={wearHat} position={[4.05, 2, -13.6]} rotation= {[0, 1, 0]}/>
           <Mask visible={wearMask} position={[4.01, 1.458, -13.57]}  rotation= {[0, 3.4, 0]}/>
           <Sunglasses visible={wearSunglasses} position={[4.05, 1.8, -13.55]}  rotation= {[0, 2.9, 0]}/>
+          <Umbrella visible={wearUmbrella} position={[4.20, 2.0, -13.55]}  rotation= {[0, 2.2, 0]}/>
       </Suspense>
 
       <Html zIndexRange={[1,5]} position={[7.5, 0, -15]} rotation-z={100}>
@@ -93,6 +96,11 @@ function ModelViewer(): React.ReactElement{
           style={{ padding: "2rem", width:"max-content", background: "#d3d3d3", fontSize: "x-large" }}
           onClick={()=> setWearMask(!wearMask)}
         > Wear mask
+        </button>
+        <button
+          style={{ padding: "2rem", width:"max-content", background: "#d3d3d3", fontSize: "x-large" }}
+          onClick={()=> setWearUmbrella(!wearUmbrella)}
+        > Wear Umbrella
         </button>
       </Html>
 
