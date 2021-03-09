635a2c3679885255cae446a4b4eb8062f07ff760
Alexandre Lascaux <alexandre.lascaux0@gmail.com>
2021-03-09 19:43:11 +0100

    :tada: button switch mode and other things than I can't remember

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,24 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { getJSON } from '../callAPI/fetch-json'
 import useRefreshablePromise from '../callAPI/use-refreshable-promise'
-import Async from '../callAPI/async';
-import Model from "./model";
+import Model, { switchModetype } from "./model";
 import { data, forecastInterface } from "../interfaces/utils";
 import { CircularProgress } from '@material-ui/core';
 
 const fetchForecastData = async (city: string) => {
-  return getJSON<data>(`https://iim-a4-back-toubonobo.herokuapp.com/api/data/forecast/${city}`)
+  return getJSON<data>(``)
  }
+ //https://iim-a4-back-toubonobo.herokuapp.com/api/data/forecas/${city}
 
 function Home(): React.ReactElement{
   const [resultData, setResultData] = useState<forecastInterface[]>();
   const defaultCity = "Paris";
   const [city, setCity] = useState<string>(defaultCity);
   const { refresh: refreshStations } = useRefreshablePromise(() => fetchForecastData(city), setResultData);
+  const [testMode, setTestMode] = useState<switchModetype | undefined>();
 
   useEffect(() => {
     refreshStations()
   }, [city]);
+
+  function loadingBeforeTestMode(){
+      setTimeout(() => {
+        setTestMode("test");
+      }, 3000);
+      return <div>
+      Waiting for data...
+      <CircularProgress />
+    </div>;
+  }
 
   return (
     <>
@@ -26,7 +37,14 @@ function Home(): React.ReactElement{
       resultData ?
       <Model data={resultData[0]} onCityClick={(city) => setCity(city)} />
       :
-      <CircularProgress />
+      <>
+      {
+        testMode ?
+        <Model mode={testMode} data={null} onCityClick={(city) => setCity(city)} />
+      :
+      loadingBeforeTestMode()
+      }
+      </>
     }
     </>
   );
--- src/views/model.tsx
+++ src/views/model.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import { Canvas, ReactThreeFiber } from 'react-three-fiber';
 import { OrbitControls, Html } from 'drei'
 import Monkey from '../component/monkey';
@@ -21,32 +21,41 @@ import Umbrella from '../component/accessories/umbrella';
 import { forecastInterface } from "../interfaces/utils";
 
 interface modelInterface{
-  data: forecastInterface,
-  onCityClick: (city: string) => void
+  data: forecastInterface | null;
+  onCityClick: (city: string) => void;
+  mode?: switchModetype;
 }
 
-function ModelViewer({data, onCityClick}: modelInterface): React.ReactElement{
-  const [storm, setStorm] = useState<boolean>(data.Precipitation.mode === "rain" && +data.Precipitation.value > 10);
-  const [sun, setSun] = useState<boolean>(data.Cloud.cover > 75);
-  const [rain, setRain] = useState<boolean>(data.Precipitation.mode === "rain");
-  const [snow, setSnow] = useState<boolean>(data.Precipitation.mode === "snow");
-  const [cloud, setCloud] = useState<boolean>(data.Cloud.cover > 0);
+export type switchModetype = "api" | "test";
+
+function ModelViewer({data, onCityClick, mode}: modelInterface): React.ReactElement{
+  const [city, setCity] = useState<"paris"|"annecy"|"lyon">("paris");
+  const isRaining = !!((data && data.Precipitation.mode === "rain") || city ==="lyon");
+  const isSnowing = !!(data && data.Precipitation.mode === "snow");
+  const hasCloud = !!((data && data.Cloud.cover > 0) || true);
+  const hasSun = !!((data && data.Cloud.cover > 75) || city !=="lyon");
+  const hasStorm = !!(data && +data.Precipitation.value > 10);
+
+
+  const [storm, setStorm] = useState<boolean>(isRaining && hasStorm);
+  const [sun, setSun] = useState<boolean>(hasSun);
+  const [rain, setRain] = useState<boolean>(isRaining);
+  const [snow, setSnow] = useState<boolean>(isSnowing);
+  const [cloud, setCloud] = useState<boolean>(hasCloud);
+
+  const [switchMode, setSwitchMode] = useState<switchModetype>(mode || "api");
 
   const [wearMask, setWearMask] = useState<boolean>(true);
-  const [wearHat, setWearHat] = useState<boolean>(wearClothes());
-  const [wearSunglasses, setWearSunglasses] = useState<boolean>(wearClothes());
-  const [wearBottle, setWearBottle] = useState<boolean>(wearClothes());
-  const [wearUmbrella, setWearUmbrella] = useState<boolean>(data.Precipitation.mode === "rain");
+  const [wearHat, setWearHat] = useState<boolean>(wearSummerClothes());
+  const [wearSunglasses, setWearSunglasses] = useState<boolean>(wearSummerClothes());
+  const [wearBottle, setWearBottle] = useState<boolean>(wearSummerClothes());
+  const [wearUmbrella, setWearUmbrella] = useState<boolean>(isRaining);
   const [camera, setCamera] = useState<Partial<ReactThreeFiber.Object3DNode<THREE.Camera, typeof THREE.Camera> & ReactThreeFiber.Object3DNode<THREE.PerspectiveCamera, typeof THREE.PerspectiveCamera> & ReactThreeFiber.Object3DNode<THREE.OrthographicCamera, typeof THREE.OrthographicCamera>>>({ far: 2000, position: [5, 1.2, -18] });
 
   console.log(data);
 
-  function rainOrNot(): boolean{
-    return rain || data.Precipitation.mode === "rain";
-  }
-
-  function umbrellaOrNot(): boolean{
-    return wearUmbrella || data.Precipitation.mode === "rain";
+  function checkIfApiModeResult(apiBool: boolean, testBool: boolean): boolean{
+    return isApiMode() ? apiBool : testBool;
   }
 
   function handleCLick() {
@@ -56,22 +65,34 @@ function ModelViewer({data, onCityClick}: modelInterface): React.ReactElement{
 
   function convertDataCloudCoverToIntensity(): number{
     return data?.Cloud.cover ? data.Cloud.cover / 10 : 1;
+   // return city ==="paris" ? 2 : 1;
   }
 
   function convertDataCloudCoverToNumber(): number{
     return data?.Cloud.cover ? data.Cloud.cover / 20 : 1;
+    //return city ==="paris" ? 5 : 1;
   }
 
   function convertDataWindSpeedToVelocity(): number{
     return data?.Wind.speed ? +data.Wind.speed / 6 : 1;
+  // return city ==="paris" ? 4 : 1;
+
   }
 
-  function wearClothes(): boolean{
-    if(+data.Temperature.feeling > 1 && data.Precipitation.mode === null){
+  function wearSummerClothes(): boolean{
+    if((data && +data.Temperature.feeling > 1 && data.Precipitation.mode === null) || city === "paris" || city === "annecy"){
       return true;
     } else {
       return false;
     }
+  }
+
+  function switchModeValue(): switchModetype {
+    return switchMode === "api" ? "test" : "api";
+  }
+
+  function isApiMode(): boolean {
+    return !!(switchMode === "api");
   }
 
   return (
@@ -90,11 +111,11 @@ function ModelViewer({data, onCityClick}: modelInterface): React.ReactElement{
       </Suspense>
 
       <Suspense fallback={null}>
-        <Sun visible={sun && !storm} />
+        <Sun visible={checkIfApiModeResult(hasSun, sun) && !storm} />
         <ambientLight visible={!storm} />
-        <Rain isVisible={rainOrNot()} rainCount={8000} />
-        <Snow isVisible={snow} snowCount={3000} />
-        <Clouds isVisible={cloud} velocity={convertDataCloudCoverToNumber()} intensity={convertDataCloudCoverToIntensity()} number={convertDataWindSpeedToVelocity()} />
+        <Rain isVisible={checkIfApiModeResult(isRaining, rain)} rainCount={8000} />
+        <Snow isVisible={checkIfApiModeResult(isSnowing,snow)} snowCount={3000} />
+        <Clouds isVisible={checkIfApiModeResult(hasCloud, cloud)} velocity={convertDataCloudCoverToNumber()} intensity={convertDataCloudCoverToIntensity()} number={convertDataWindSpeedToVelocity()} />
         <Flamingo scale={[0.3, 0.3, 0.3]} />
         <Parrot scale={[0.3, 0.3, 0.3]} />
         <Stork scale={[0.3, 0.3, 0.3]} />
@@ -104,15 +125,21 @@ function ModelViewer({data, onCityClick}: modelInterface): React.ReactElement{
       </Suspense>
 
       <Suspense fallback={null}>
-          <Hat visible={wearClothes()} position={[4.05, 2, -13.6]} rotation= {[0, 1, 0]}/>
+          <Hat visible={checkIfApiModeResult(wearSummerClothes(), wearHat)} position={[4.05, 2, -13.6]} rotation= {[0, 1, 0]}/>
           <Mask visible={wearMask} position={[4.01, 1.458, -13.57]}  rotation= {[0, 3.4, 0]}/>
-          <Sunglasses visible={wearClothes()} position={[4.02, 1.8, -13.54]}  rotation= {[0, 2.8, 0]}/>
-          <WaterBottle visible={wearClothes()} position={[4.97, 1.3, -13.2]}  rotation= {[0, 2.9, 0]}/>
-          <Umbrella visible={umbrellaOrNot()} position={[3.10, 1.25, -13.70]}  rotation= {[0, 2.2, 0]}/>
+          <Sunglasses visible={checkIfApiModeResult(wearSummerClothes(), wearSunglasses)} position={[4.02, 1.8, -13.54]}  rotation= {[0, 2.8, 0]}/>
+          <WaterBottle visible={checkIfApiModeResult(wearSummerClothes(), wearBottle)} position={[4.97, 1.3, -13.2]}  rotation= {[0, 2.9, 0]}/>
+          <Umbrella visible={checkIfApiModeResult(isRaining, wearUmbrella)} position={[3.10, 1.25, -13.70]}  rotation= {[0, 2.2, 0]}/>
       </Suspense>
 
 
-      <Html zIndexRange={[1,5]} scaleFactor={7} position={[7.5, 0.5, -15]} rotation-z={100}>
+      <Html scaleFactor={13} position={[8.3, 3.25, -13.5]} rotation-z={100}>
+      <GradientBtn label={`Switch to ${switchModeValue()} mode`} onClick={()=> setSwitchMode(switchModeValue())} />
+  
+      </Html>
+
+  
+      <Html style={{display: switchMode === "test" ? "initial" : "none"}} zIndexRange={[1,5]} scaleFactor={7} position={[7.5, 0.5, -15]} rotation-z={100}>
       <GradientBtn label={<span role="img" aria-label="storm"> Sun  ☀️</span>} onClick={() => setSun(!sun)} />
       <GradientBtn label={<span role="img" aria-label="storm"> Clouds  ☁️</span>} onClick={() => setCloud(!cloud)} />
       <GradientBtn label={<span role="img" aria-label="storm"> Snow  ❄️</span>} onClick={() => setSnow(!snow)} />
@@ -120,7 +147,7 @@ function ModelViewer({data, onCityClick}: modelInterface): React.ReactElement{
       <GradientBtn label={<span role="img" aria-label="storm"> Storm  !!⚡</span>} onClick={handleCLick} />
       </Html>
 
-      <Html scaleFactor={6} position={[4.25, -0.75, -13.5]} rotation-z={100}>
+      <Html style={{display: switchMode === "test" ? "initial" : "none"}} scaleFactor={6} position={[4.25, -0.75, -13.5]} rotation-z={100}>
       <GradientBtn label={"Wear hat"} onClick={()=> setWearHat(!wearHat)} />
       <GradientBtn label={"Wear Sunglasses"} onClick={()=> setWearSunglasses(!wearSunglasses)} />
       <GradientBtn label={"Wear Mask"} onClick={()=> setWearMask(!wearMask)} />
@@ -128,10 +155,18 @@ function ModelViewer({data, onCityClick}: modelInterface): React.ReactElement{
       <GradientBtn label={"Wear Umbrella"} onClick={()=> setWearUmbrella(!wearUmbrella)} />
       </Html>
 
-      <Html scaleFactor={7} position={[1, -0.75, -15.5]} rotation-z={100}>
-      <GradientBtn label={"Paris"} onClick={()=> onCityClick("Paris")} />
-      <GradientBtn label={"Lyon"} onClick={()=> onCityClick("Lyon")} />
-      <GradientBtn label={"Annecy"} onClick={()=> onCityClick("Annecy")} />
+      <Html style={{display: switchMode === "api" ? "initial" : "none"}} scaleFactor={7} position={[1, -0.75, -15.5]} rotation-z={100}>
+{
+  /*
+   <GradientBtn label={"Paris"} onClick={()=> onCityClick("Paris")} />
+   <GradientBtn label={"Lyon"} onClick={()=> onCityClick("Lyon")} />
+   <GradientBtn label={"Annecy"} onClick={()=> onCityClick("Annecy")} />
+   */
+}
+    <div>{city}</div>
+    <GradientBtn disabled={city === "paris"} label={"Paris"} onClick={()=> setCity("paris")} />
+    <GradientBtn disabled={city === "lyon"} label={"Lyon"} onClick={()=> setCity("lyon")} />
+    <GradientBtn disabled={city === "annecy"} label={"Annecy"} onClick={()=> setCity("annecy")} />
       </Html>
 
       <Html position={[4.5, -0.2, -13.5]} rotation-z={100}>
