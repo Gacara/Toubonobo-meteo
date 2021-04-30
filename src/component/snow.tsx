44dfbbaf4ccc2ec9611d4307eb6735033533fee0
Romain Goulet <romain.goulet@edu.devinci.fr>
2021-04-30 17:40:50 +0200

    add zoom

--- src/component/snow.tsx
+++ src/component/snow.tsx
@@ -12,9 +12,9 @@ const setInitialPositions = (snowCount: number) => {
     const initialVelocities = [];
     const initialAccelerations = [];
     for (let i = 0; i < snowCount; i++) {
-      initialPositions.push(-15 + Math.random() * 40);
+      initialPositions.push(-12 + Math.random() * 40);
       initialPositions.push(Math.random() * 10);
-      initialPositions.push(-17 + Math.random() * 5);
+      initialPositions.push(-18.5 + Math.random() * 5);
       initialVelocities.push(0);
       initialVelocities.push(-0.5);
       initialVelocities.push(0);
