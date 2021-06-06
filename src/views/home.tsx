56e5ad73938711a04883d128e4a008907bdd1aa2
Romain Goulet <romain.goulet@edu.devinci.fr>
2021-06-06 11:41:35 +0200

    fix date

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -19,6 +19,7 @@ function Home(): React.ReactElement{
   useEffect(() => {
     refreshData()
   }, [city]);
+  console.log(resultData)
 
   function loadingBeforeTestMode(){
       setTimeout(() => {
