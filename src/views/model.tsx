60893b7efea9bc61c7232b1f223f0e1f3a1d6400
Romain Goulet <romain.goulet@edu.devinci.fr>
2021-08-17 06:12:40 +0200

    remove shoes

--- src/views/model.tsx
+++ src/views/model.tsx
@@ -472,9 +472,11 @@ function renderLoadingScreen(){
 
       <Suspense fallback={null}>
           <WaterBottle visible={wearablesVariables.wearBottle} position={[4.68, 1.36, -13.15]}  rotation= {[0, 2.9, 0]}/>
-          <CatShoe visible={wearablesVariables.wearShoes}scale={[0.48,0.48,0.48]} position={[3.8, -0.015, -13.65]} rotation= {[-0.1, 1.5, 0]}/>
-          <CatShoe visible={wearablesVariables.wearShoes} scale={[0.48,0.48,0.48]} position={[4.27, -0.015, -13.65]} rotation= {[-0.1, 1, 0]}/>
-          
+
+          {
+          //<CatShoe visible={wearablesVariables.wearShoes}scale={[0.48,0.48,0.48]} position={[3.8, -0.015, -13.65]} rotation= {[-0.1, 1.5, 0]}/>
+          //<CatShoe visible={wearablesVariables.wearShoes} scale={[0.48,0.48,0.48]} position={[4.27, -0.015, -13.65]} rotation= {[-0.1, 1, 0]}/>
+          }
       </Suspense>
 
     
